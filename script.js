bb4e16a343af1893c949e7ee5d7695a977c8a3ec
Rakhmonali7 <zamonalievr7@gmail.com>
2023-08-03 19:58:45 +0900

    create a toggle and submit

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-
+let map, mapEvent;
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
@@ -18,41 +18,47 @@ if (navigator.geolocation)
       const { longitude } = position.coords;
       const coords = [latitude, longitude];
       // following piece of code from Leaflet Library OVERVIEW  SECTION
-      const map = L.map('map').setView(coords, 14); // 'map' in L.map is ID map div in html where map is displayed. coords is veriable where latitude and longitude is assigned and 13 is the scale of the map.
+      map = L.map('map').setView(coords, 14); // 'map' in L.map is ID map div in html where map is displayed. coords is veriable where latitude and longitude is assigned and 13 is the scale of the map.
       console.log(map);
       L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.googlemap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      // T HE END OF CODE FROEM LEAFLET LIBRARY
-
-      map.on('click', function (mapEvent) {
+      // THE END OF CODE FROEM LEAFLET LIBRARY
+      //hendling clicks on map
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
         form.classList.remove('hidden');
         inputDistance.focus();
-
       });
     },
     function () {
       alert('Could not get your position!'); // created two functions first one if location got successful, Second function if it's failed to get location
     }
   );
-  form.addEventListener('submit', fumction(){
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  // clearing value of input fields in the form
+  inputDistance.value = inputDuration.value = inputCadence.value = '';
+  // Display marker
+  const { lat, lng } = mapEvent.latlng;
 
-    const { lat, lng } = mapEvent.latlng;
-
-    L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(
-        L.popup({
-            maxWidth: 250,
-            maxHeight: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup',
-        })
-        )
-        .setPopupContent('Workout')
-        .openPopup();
-
-  })
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        maxHeight: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
