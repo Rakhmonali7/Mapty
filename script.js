3c8d907d57095db12e7f9767f2ceea9cee5cb25e
Rakhmonali7 <zamonalievr7@gmail.com>
2023-08-03 17:20:05 +0900

    create a form

--- script.js
+++ script.js
@@ -11,42 +11,48 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-if(navigator.geolocation)
-    navigator.geolocation.getCurrentPosition(function(position){
-        const {latitude} = position.coords
-        const {longitude} = position.coords
-        
-        console.log(`https://www.google.pt/maps/@${latitude}, ${longitude}`);
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      const coords = [latitude, longitude];
+      // following piece of code from Leaflet Library OVERVIEW  SECTION
+      const map = L.map('map').setView(coords, 14); // 'map' in L.map is ID map div in html where map is displayed. coords is veriable where latitude and longitude is assigned and 13 is the scale of the map.
+      console.log(map);
+      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.googlemap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
 
-        const coords = [latitude, longitude]
-        const map = L.map('map').setView(coords, 13);
+      // T HE END OF CODE FROEM LEAFLET LIBRARY
 
-        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+      map.on('click', function (mapEvent) {
+        form.classList.remove('hidden');
+        inputDistance.focus();
 
-    map.on('click', function(mapEvent){
-        form.classList.remove('hidden')
-        inputDistance.focus()
-    //     console.log(mapEvent);
-    //     const {lat, lng} = mapEvent.latlng
-    //     L.marker([lat, lng]).addTo(map)
-    // .bindPopup(L.popup({
-    //     maxWidth: 250,
-    //     minWidth: 100,
-    //     autoClose: false,
-    //     closeOnClick: false,
-    //     className: 'running-popup'
-    // })).setPopupContent('Workout').openPopup();                                          
-    })
+      });
+    },
+    function () {
+      alert('Could not get your position!'); // created two functions first one if location got successful, Second function if it's failed to get location
+    }
+  );
+  form.addEventListener('submit', fumction(){
 
+    const { lat, lng } = mapEvent.latlng;
 
+    L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+        L.popup({
+            maxWidth: 250,
+            maxHeight: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+        })
+        )
+        .setPopupContent('Workout')
+        .openPopup();
 
-    }, function(){
-        alert("Could not get your position")
-    })
-
-    form.addEventListener('submit', function(){
-        
-    })
+  })
