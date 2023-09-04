9499b16da83d89b1e0cc74aee08970c311c35564
Rakhmonali7 <zamonalievr7@gmail.com>
2023-09-04 21:14:40 +0900

    working on the structure of the code

--- script.js
+++ script.js
@@ -10,55 +10,107 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-let map, mapEvent;
-if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      const coords = [latitude, longitude];
-      // following piece of code from Leaflet Library OVERVIEW  SECTION
-      map = L.map('map').setView(coords, 14); // 'map' in L.map is ID map div in html where map is displayed. coords is veriable where latitude and longitude is assigned and 13 is the scale of the map.
-      console.log(map);
-      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.googlemap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
 
-      // THE END OF CODE FROEM LEAFLET LIBRARY
-      //hendling clicks on map
-      map.on('click', function (mapE) {
-        mapEvent = mapE;
-        form.classList.remove('hidden');
-        inputDistance.focus();
-      });
-    },
-    function () {
-      alert('Could not get your position!'); // created two functions first one if location got successful, Second function if it's failed to get location
-    }
-  );
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  // clearing value of input fields in the form
-  inputDistance.value = inputDuration.value = inputCadence.value = '';
-  // Display marker
-  const { lat, lng } = mapEvent.latlng;
+class Workout {
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.duration = duration;
+    this.distance = distance;
+  }
+}
+class Runnig extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
 
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        maxHeight: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup',
-      })
-    )
-    .setPopupContent('Workout')
-    .openPopup();
-});
-inputType.addEventListener('change', function () {
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-});
+class App {
+  #map;
+  #mapEvent;
+  constructor() {
+    this._getPosition();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
+
+  _getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('Could not get your position!'); // created two functions first one if location got successful, Second function if it's failed to get location
+        }
+      );
+  }
+
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    const coords = [latitude, longitude];
+    // following piece of code from Leaflet Library OVERVIEW  SECTION
+    this.#map = L.map('map').setView(coords, 14); // 'map' in L.map is ID map div in html where map is displayed. coords is veriable where latitude and longitude is assigned and 13 is the scale of the map.
+
+    L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.googlemap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    // THE END OF CODE FROEM LEAFLET LIBRARY
+    //hendling clicks on map
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+    // clearing value of input fields in the form
+    inputDistance.value = inputDuration.value = inputCadence.value = '';
+    // Display marker
+    console.log(this.#mapEvent);
+    const { lat, lng } = this.#mapEvent.latlng;
+
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          maxHeight: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('Workout')
+      .openPopup();
+  }
+}
+
+const app = new App();
